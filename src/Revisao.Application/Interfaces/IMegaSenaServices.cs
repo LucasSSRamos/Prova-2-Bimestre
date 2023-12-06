2c833f46f8c309af54527e550268ac0850a07f37
Lucas Ramos <110167908+LucasSSRamos@users.noreply.github.com>
2023-12-06 20:13:03 -0300

    Add files via upload

---
+++ src/Revisao.Application/Interfaces/IMegaSenaServices.cs
@@ -0,0 +1,16 @@
+﻿using Revisao.Application.ViewModels.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revisao.Application.Interfaces
+{
+    public interface IMegaSenaServices
+    {
+        Task<IEnumerable<MegaSenaViewModel>> ObterTodosOsJogos(MegaSenaViewModel megaSena);
+
+        void RegistrarJogo(NovoRegistroMegaSenaViewModel megasena);
+    }
+}
