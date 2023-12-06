ba4741a66c8f8d9d5d104b9715ac972bc129be82
Lucas Ramos <110167908+LucasSSRamos@users.noreply.github.com>
2023-12-06 20:23:58 -0300

    Add files via upload

---
+++ src/Revisao.Domain/Interfaces/IMegaSenaRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Revisao.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revisao.Domain.Interfaces
+{
+    public interface IMegaSenaRepository
+    {
+        Task<IEnumerable<JogoMegaSena>> ObterTodosOsJogos();
+
+        void RegistrarJogo(JogoMegaSena megasena);
+    }
+}
